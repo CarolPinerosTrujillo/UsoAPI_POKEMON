5b289c91cb59469ea5e423f3a6dc73d4c95039d6
CarolPinerosTrujillo <caroll25m@gmail.com>
2026-07-06 15:04:38 -0500

    intento de avance

--- explorar.js
+++ explorar.js
@@ -2,31 +2,61 @@ const prompt = require("prompt-sync")(); // configurar entrada de datos
 
 async function obtenerPokemon() {
   const nombre = prompt("Digita pokemon: ");
-  const respuesta = await fetch('https://pokeapi.co/api/v2/pokemon/'+nombre);
+  
+  //encapsular funcion
+  const url='https://pokeapi.co/api/v2/pokemon/'+nombre
+
+  const respuesta = await fetch(url);
   const datos = await respuesta.json();
-  return;
   //console.log(datos);
   //console.log(respuesta);
-  
   //console.log(datos.types[0].type.name);
-
    if (!respuesta.ok) {
-    console.log("No se pudo consultar la tasa. Código:", respuesta.status);
-    return;
+    console.log("No se pudo consultar el pokemon. Código:", respuesta.status);
+    return null;
+  }
+  // tipos
+  for (let t of datos.types) {
+    console.log("Tipo:", t.type.name);
   }
 
+  // stats
+  for (let s of datos.stats) {
+    console.log("Stat:", s.stat.name, "-", s.base_stat);
+  }
+
+  // habilidades
+  for (let a of datos.abilities) {
+    console.log("Habilidad:", a.ability.name);
+  }
 }
 
+obtenerPokemon() 
 
-async function buscarPokemon(nombre) {
-  const p = await obtenerPokemon(nombre);
-  console.log("Nombre:", p.name);
-  console.log("Altura:", p.height);
-  console.log("Peso", p.weight);
-  console.log("Tipos:", p.types.map(t => t.type.name));
-  console.log("Stats:", p.stats.map(s => `${s.stat.name}: ${s.base_stat}`));
+
+
+
+async function buscarPokemon() {
+
+  const pokemonLeido = obtenerPokemon(nombre)
+  return 
+
+
+
+}
+
+//buscarPokemon();
+
+// async function buscarPokemon(nombre) {
+//   const p = await obtenerPokemon(nombre);
+//   console.log("Nombre:", p.name);
+//   console.log("Altura:", p.height);
+//   console.log("Peso", p.weight);
+//   console.log("Tipos:", p.types.map(t => t.type.name));
+//   console.log("Stats:", p.stats.map(s => `${s.stat.name}: ${s.base_stat}`));
   
-}
+// }
 
-obtenerPokemon();
-buscarPokemon();
+
+
+//obtenerPokemon();
